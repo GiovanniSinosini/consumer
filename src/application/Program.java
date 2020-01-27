4ea784495e2f49d65cc47dadf170c9eb342ed717
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-27 18:10:01 +0000

    Declared lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -16,8 +17,12 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		list.forEach(Product::nonStaticPriceUpDate);
+		double factor = 1.1;
 		
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
+
+		
+		list.forEach(cons);
 		list.forEach(System.out::println);
 		
 	}
